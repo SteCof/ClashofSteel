7691dc570cdbe513b71ffc49b899c90221fec09a
Joe McGuire <josephpaulmcguire@gmail.com>
2012-10-14 23:40:36 +0100

    Added Card display in HTML and CSS with support for an image.

--- ClashOfSteel.js
+++ ClashOfSteel.js
@@ -1,8 +1,11 @@
 var diagonalCut = new Card({
   "name": "Diagonal Cut",
   "resTime": "8",
-  "type": "strike"
+  "type": "strike",
+  "image": "http://fc00.deviantart.net/fs70/f/2011/322/9/e/shieldmaiden_sketch_by_doranbladefist-d4gkyug.png",
 });
 
 console.log(diagonalCut.getName() + " is the first card!");
-console.log("Tt's a " + diagonalCut.getType() + " with resolution time " + diagonalCut.getResTime())
+console.log("Tt's a " + diagonalCut.getType() + " with resolution time " + diagonalCut.getResTime());
+
+diagonalCut.display();
