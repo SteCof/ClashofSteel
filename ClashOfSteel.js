bbd861e430bd9d84e46178529a1492d53584ed1a
SteCof <reaperskill@gmail.com>
2012-10-15 00:01:17 +0100

    Displaying second card

--- ClashOfSteel.js
+++ ClashOfSteel.js
@@ -13,7 +13,8 @@ var slam= new Card({
 	"name": "Slam",
 	"resTime": "7",
 	"type": "strike",
+	"image": "http://fc00.deviantart.net/fs70/f/2011/322/9/e/shieldmaiden_sketch_by_doranbladefist-d4gkyug.png",
 	});
 
 diagonalCut.display();
-
+slam.display();
