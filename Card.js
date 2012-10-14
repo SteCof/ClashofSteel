dce0ccfe706a8de139de235b62da4acd23970f1f
SteCof <reaperskill@gmail.com>
2012-10-14 22:49:48 +0100

    Added some more details to cards
    Added in the resTime and type properties

--- Card.js
+++ Card.js
@@ -1,9 +1,15 @@
 var Card = function(settings) {
   var name = settings.name;
-  
+  var resTime = settings.resTime;
+  var type = settings.type
   this.getName = function() {
     return name;
   }
-
+  this.getResTime = function() {
+    return resTime;
+  }
+  this.getType = function() {
+    return type;
+  }
   return this;
 }
--- ClashOfSteel.js
+++ ClashOfSteel.js
@@ -1,5 +1,8 @@
 var diagonalCut = new Card({
-  'name': 'Diagonal Cut',
+  "name": "Diagonal Cut",
+  "resTime": "8",
+  "type": "strike"
 });
 
-console.log(diagonalCut.getName() + ' is the first card!');
+console.log(diagonalCut.getName() + " is the first card!");
+console.log("Tt's a " + diagonalCut.getType() + " with resolution time " + diagonalCut.getResTime())
