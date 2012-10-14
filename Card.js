05d93ffd46276decde96232b0df76b9adf27c523
SteCof <reaperskill@gmail.com>
2012-10-14 23:57:17 +0100

    Manually merging (Joe McGuire's pull request)
    https://github.com/JoeMcGuire/ClashofSteel/commit/4aeed7dbf5ff3e6b59f0f7d1eba4981ee6bbed6e
    With current version

--- Card.js
+++ Card.js
@@ -2,6 +2,7 @@ var Card = function(settings) {
   var name = settings.name;
   var resTime = settings.resTime;
   var type = settings.type
+  var image = settings.image
   this.getName = function() {
     return name;
   }
@@ -10,6 +11,21 @@ var Card = function(settings) {
   }
   this.getType = function() {
     return type;
+  }
+  this.getImage = function() {
+    return image;
+  }
+  this.display = function() {
+    var displayCard = document.createElement('article');
+    var displayImg = document.createElement('img');
+    displayImg.src = this.getImage();
+    var displayHeading = document.createElement('h1');
+    displayCard.className = "clash-of-steel-card";
+    var displayName = document.createTextNode(this.getName());
+    displayHeading.appendChild(displayName);
+    displayCard.appendChild(displayHeading);
+    displayCard.appendChild(displayImg);
+    document.body.appendChild(displayCard);
   }
   return this;
 }
--- ClashOfSteel.js
+++ ClashOfSteel.js
@@ -1,14 +1,19 @@
 var diagonalCut = new Card({
   "name": "Diagonal Cut",
   "resTime": "8",
-  "type": "strike"
+  "type": "strike",
+  "image": "http://fc00.deviantart.net/fs70/f/2011/322/9/e/shieldmaiden_sketch_by_doranbladefist-d4gkyug.png",
 });
 
 console.log(diagonalCut.getName() + " is the first card!");
 console.log("Tt's a " + diagonalCut.getType() + " with resolution time " + diagonalCut.getResTime());
 
-var slam= new card({
+
+var slam= new Card({
 	"name": "Slam",
-	"resTime": "7"
-	"type": "strike"
+	"resTime": "7",
+	"type": "strike",
 	});
+
+diagonalCut.display();
+
