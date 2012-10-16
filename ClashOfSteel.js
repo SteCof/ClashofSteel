1991f211a46d089d1e17785a77990487b59f4383
SteCof <reaperskill@gmail.com>
2012-10-16 14:31:18 +0100

    Capitalised card types.

--- ClashOfSteel.js
+++ ClashOfSteel.js
@@ -1,7 +1,7 @@
 var diagonalCut = new Card({
   "name": "Diagonal Cut",
   "resTime": "8",
-  "type": "strike",
+  "type": "Strike",
   "image": "http://fc00.deviantart.net/fs70/f/2011/322/9/e/shieldmaiden_sketch_by_doranbladefist-d4gkyug.png",
 });
 
@@ -12,14 +12,14 @@ console.log("Tt's a " + diagonalCut.getType() + " with resolution time " + diago
 var slam= new Card({
 	"name": "Slam",
 	"resTime": "7",
-	"type": "strike",
+	"type": "Strike",
 	"image": "http://fc00.deviantart.net/fs70/f/2011/322/9/e/shieldmaiden_sketch_by_doranbladefist-d4gkyug.png",
 	});
 
 var superLongCardNameCardForSomeReason= new Card({
 	"name": "Super Long Card Name Card For Some Reason",
 	"resTime": "10",
-	"type": "defence",
+	"type": "Defence",
 	"image": "http://fc00.deviantart.net/fs70/f/2011/322/9/e/shieldmaiden_sketch_by_doranbladefist-d4gkyug.png",
 	});
 	
