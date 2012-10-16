cd770e02eb709068360dce4fd53df5b9b0de1a8f
SteCof <reaperskill@gmail.com>
2012-10-16 12:38:40 +0100

    Removed displayType
    I couldn't simplify the HTML file with it, so it ws unneeded complexity.
    Hopefully I'll learn how soon, but am without internet access at
    present.

--- Card.js
+++ Card.js
@@ -18,23 +18,21 @@ var Card = function(settings) {
   }
   this.display = function() {
     var displayCard = document.createElement('article');
-	var displayType=document.createElement('article');
     var displayImg = document.createElement('img');
     displayImg.src = this.getImage();
     var displayHeading = document.createElement('h1');
 	var displayHeadingName = document.createElement('span');
 	var displayResTime = document.createElement('span');
 	displayHeadingName.className="clash-of-steel-heading-name";
-    displayType.className = "clash-of-steel-"+this.getType();
+    displayCard.className = "clash-of-steel-"+this.getType();
     displayResTime.className = "clash-of-steel-res-time";
     var displayName = document.createTextNode(this.getName());
 	displayResTime.appendChild(document.createTextNode(" " + this.getResTime()));
     displayHeadingName.appendChild(displayName);
 	displayHeading.appendChild(displayHeadingName);
 	displayHeading.appendChild(displayResTime);
-    displayType.appendChild(displayHeading);
-    displayType.appendChild(displayImg);
-	displayCard.appendChild(displayType)
+    displayCard.appendChild(displayHeading);
+    displayCard.appendChild(displayImg);
     document.body.appendChild(displayCard);
   }
   return this;
